7a3e050f0dba1c094e60418c5f1dc6b676dda82f
Mattia Monga <mattia.monga@unimi.it>
2024-07-30 08:38:07 +0200

    solution

--- exam.py
+++ exam.py
@@ -42,28 +42,60 @@ import arviz as az              # type: ignore
 #
 # Load the data in a pandas dataframe using the `MouseID` column as the index.
 
-pass
+data = pd.read_csv('mice.csv', index_col='MouseID')
+
+data.head()
 
 # ### Exercise 2 (max 3 points)
 #
 # Plot the histograms (the number of bins should be 20 in both cases) of the `NR1_N` and `NR2A_N` values on the same figure.
 # Add a legend to distinguish the plots.
 
-pass
+# +
+fig, ax = plt.subplots()
+
+for p in ('NR1_N', 'NR2A_N'):
+    data[p].hist(ax=ax, bins=20, label=p, alpha=.7)
+_ = fig.legend()
+# -
 
 # ### Exercise 3 (max 3 points)
 #
 # Make a figure with two columns of plots. In the first column plot together (contrast) the histograms of `NR2A_N` for the two genotypes in the dataset. In the second column plot together (contrast) the histograms (20 bins) of `NR2A_N` for the two treatments in the dataset. Use density histograms to make the diagrams easy to compare; add proper titles and legends.
 #
 
-pass
+# +
+fig, ax = plt.subplots(ncols=2)
+
+
+col = 'NR2A_N'
+for i, f in enumerate(['Genotype', 'Treatment']):
+    for g in data[f].unique():
+        ax[i].hist(data[data[f] == g][col], density=True, bins=20, label=g, alpha=.7)
+        ax[i].legend()
+        ax[i].set_title(f)
+fig.tight_layout()
+# -
 
 # ### Exercise 4 (max 5 points)
 #
 # Make a (huge) figure with the histograms plotted in the previous exercise for all the proteins whose name starts with 'p' (there are 22 such columns), each in a different row of the figure (the figure will have 2 columns and 22 rows; to make it readable set the `figsize` to `(5, 3*22)`). 
 #
 
-pass
+# +
+fig, ax = plt.subplots(ncols=2, nrows=22, figsize=(5, 3*22))
+
+
+for j, col in enumerate([c for c in data.columns if c.startswith('p')]):
+    for i, f in enumerate(['Genotype', 'Treatment']):
+        for g in data[f].unique():
+            ax[j, i].hist(data[data[f] == g][col], density=True, bins=20, label=g, alpha=0.7)
+            ax[j, i].legend()
+            ax[j, i].set_title(f'{col} ({f})')
+fig.tight_layout()
+
+
+# -
 
 # ### Exercise 5 (max 7 points)
 #
@@ -71,13 +103,35 @@ pass
 #
 # To get the full marks, you should declare correctly type hints and add a test within a doctest string.
 
-pass
+def evodd_digits(x: float, even: bool = True) -> str:
+    """Return all the even (or odd if even is False) digits of the decimal representation of the number x.
+    
+    >>> evodd_digits(1.2345)
+    '24'
+
+    >>> evodd_digits(1.2345, False)
+    '135'
+
+    >>> evodd_digits(0.2345)
+    '024'
+    
+    """
+    ris = ''
+    for c in [d for d in str(x) if d.isnumeric()]:
+        if even and int(c) % 2 == 0:
+            ris += c
+        elif not even and int(c) % 2 != 0:
+            ris += c
+        else:
+            pass
+    return ris
+
 
 # +
 # You can test your docstrings by uncommenting the following two lines
 
-# import doctest
-# doctest.testmod()
+import doctest
+doctest.testmod()
 # -
 
 # ### Exercise 6 (max 5 points)
@@ -87,13 +141,32 @@ pass
 # To get full marks, avoid the use of explicit loops.
 #
 
-pass
+data['NR_evodd'] = data['NR1_N'].map(str).str.cat(data['NR2A_N'].map(str)).map(evodd_digits)
+
 
 # ### Exercise 7 (max 4 points)
 #
 # Draw the scatterplot of the standardized values of `'Bcatenin_N'` vs. (also standardized) `'Tau_N'` for the class 't-CS-s'. The standard value $z$ corresponding to a value $v$ taken from a series with mean $\bar v$ and standard deviation $\sigma$ is: $z = \frac{v - \bar v}{\sigma}$.
 
-pass
+# +
+def standardize(x: pd.Series) -> pd.Series:
+    """For each value assess its distance from the mean, w.r.t. the standard deviation.
+
+    >>> standardize(pd.Series([-1, 0, 1])).tolist() # mean: 0, devstd: 1
+    [-1.0, 0.0, 1.0]
+    """
+    return (x - x.mean()) / x.std()
+
+fig, ax = plt.subplots()
+d = data[data['class'] == 't-CS-s']
+
+xd = standardize(d['Bcatenin_N'])
+yd = standardize(d['Tau_N'])
+
+ax.scatter(xd, yd)
+ax.set_title('Tau_N vs. Bcatenin_N for t-CS-s')
+fig.tight_layout()
+# -
 
 # ### Exercise 8 (max 4 points)
 #
@@ -106,4 +179,13 @@ pass
 #
 # Code this model with pymc, sample the model, and print the summary of the resulting estimation by using `az.summary`. 
 
-pass
+with pm.Model() as model:
+    a = pm.Normal('alpha', mu=0, sigma=0.2)
+    b = pm.Normal('beta', mu=0, sigma=0.5)
+    l = pm.Exponential('sigma', 1)
+
+    pm.Normal("t", mu=a+b*xd, sigma=l, observed=yd)
+
+    idata = pm.sample()
+az.summary(idata)
+
